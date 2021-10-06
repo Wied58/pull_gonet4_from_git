8a50e6bd7f6a76c21b3f5297838b0673f40c1f03
Jeff Wiedemann <fej158@gmail.com>
2021-10-07 00:55:48 +0100

    added decorate to git command

--- fetch_git_hash.sh
+++ fetch_git_hash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd /home/pi/Tools/git_repo
-VER=`git log --pretty=oneline | head -1 | awk '{print substr($1,1,5)}'`
+cd /home/pi/Tools/git
+VER=`git log --pretty=oneline --decorate | head -1 | awk '{print substr($1,1,5)}'`
 rm -f /home/pi/Tools/Version/*
 touch /home/pi/Tools/Version/$VER
 
